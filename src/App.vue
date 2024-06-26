e69c0c2688160a655fc1b5cad2fc9e1fecd0dda3
sawyeyintag <5352sawyeyintaung@gmail.com>
2024-06-26 23:54:20 +0700

    Setup Project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div class="text-red-500">Hello</div>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import './style.css'
+import lara from './presets/lara'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(PrimeVue, {
+  unstyled: true,
+  pt: lara
+})
+
+app.mount('#app')
